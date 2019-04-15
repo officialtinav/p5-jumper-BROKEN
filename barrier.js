fd2e1bd8509e10de1ebf0bb65feca834df904dc6
tina <34047417+officialtinav@users.noreply.github.com>
2019-04-15 12:05:51 -1000

    Add files via upload

--- barrier.js
+++ barrier.js
@@ -1,10 +1,4 @@
-var w = window.innerWidth;
-var h = window.innerHeight;
-var barrier = new barrier();
-var barrierW = 100;
-var barrierH = 30;
-
-    function barrier() {
+function barrier() {
       this.x = w;
       this.y = 0;
       this.gravity = -0.9; //the force of gravity
@@ -12,25 +6,72 @@ var barrierH = 30;
       this.velocity = 0; //velocity of player
       
       this.show = function() {
-        fill(color('white'));
-        rect(this.x,this.y,barrierW,barrierH);
+        fill(color('green'));
+        rect(this.x,this.y,100,200);
+        if (this.x > 0){ // if reached the right edge of the canvas, go back to the left edge
+  	    x = 0;
+        }
       }
 
-      
-      this.update = function() {
-        this.velocity += this.gravity; //gravity applied when not jumping
-        this.x += this.velocity;
-        this.velocity *= 0.8; //air resistance
-       
-       if (this.x < 0) {
-         this.x = w;
-         this.y = h;
-         this.y = random(h);
-         this.gravity = random(2,5)
-         barrierH = random(50,100)
-         barrierW = random(50,200)
+       this.update= function() {
+         this.velocity += this.gravity; //applied when not jumping
+         this.x += this.velocity; //position on the y axis
+         this.velocity *= 0.2; //air resistance when the ball drops down
+         
+         if(this.x< 0 -50) {
+           this.y = Math.floor(Math.random()*h-50);
+           
+           this.velocity = 0; //gravity
+           this.x = w;
+           this.gravity--;
+             if(this.gravity<-5){
+               this.gravity=-5;
+             }
+           score++;
+             if((score>5)&&(score<10)){
+               
+               /* if(this.x < - 50) {
+                 this.velocity = 0;
+                 this.y = Math.floor(Math.random() * h + 100);
+                 this.x = w + 50;
+               } */
+             }
+         }
        }
-      }
-    } //object ends here
+}
+
+
+
+function barrier2() {
+       this.x= w;
+       this.y= 100;
+       this.gravity= -1; //force of gravity
+       this.lift= -10; //opposite
+       this.velocity= 0; //velocity of player
+      
+       this.show= function() {
+         fill(color('green'));
+         rect(this.x, this.y, 100, 200);
+         strokeWeight(0);
+       }
+      
+       this.update= function() {
+         this.velocity += this.gravity; //gravity when not jumping
+         this.x += this.velocity; //changing the position on the y axis
+         this.velocity *= 0.8; //air resistance when the ball drops down
+         
+         if(this.x<5) {
+           this.y = Math.floor(Math.random()*h-50);
+           this.velocity = 0;
+           this.x = w;
+           score++;
+         }
+       }
+}
+
     
- 
+    function keyPressed() {
+      if (keyCode === 32) {
+        jumper.up();
+      }
+    }
--- jumper.js
+++ jumper.js
@@ -1,8 +1,13 @@
+var girl; //globla variable for character
 
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    var jumper = new jumper();
-    
+function preload() {
+  girl = loadImage("female.png");
+}
+
+function draw() {
+  image(girl, this.x, this.y);
+}
+
     function jumper() {
       this.x = 50;
       this.y = 0;
@@ -11,37 +16,61 @@
       this.velocity = 0; //velocity of player
       
       this.show = function() {
-        fill(color("red"));
-        ellipse(this.x,this.y,50,50);
+       /* fill(color('red'));
+        ellipse(this.x,this.y,50,50); */
         
+        //image(characterLeft (variable name), jumper.xx and y position
+        
+       /* image(girl, this.x,this.y); */
       }
       this.up = function() {
-        this.velocity += this.lift; //jumping function
+        this.velocity += this.lift; //jump
       }
-      
-      
+     
       this.update = function() {
-        this.velocity += this.gravity; //gravity applied when not jumping
+        this.velocity += this.gravity; //gravity
         this.y += this.velocity;
         this.velocity *= 0.8; //air resistance
-        if (this.y > h) { //jumper hits the floor
+        if (this.y > h) { //hits the floor
           this.y = h;
           this.velocity = 0;
         }
-        if (this.y < 0) { //jumper hit the ceiling
+        if (this.y < 0) { //hits the ceiling
           this.y = 0;
           this.velocity = 0;
         }
+        if (keyIsDown(37)) { //left
+          this.x -= 5;
+        }
+      
+        if (keyIsDown(39)) { //right
+          this.x += 5;
+        }
       }
+
+      
     } //object ends here
+    
+
     
     function keyPressed() {
       if (keyCode === 32) {
         jumper.up();
       }
-      
-       if (keyCode === 39) {
+
+    /*   if (keyCode === 39) {
         jumper.right();
       }
+      
+      if (keyCode === 37) {
+        jumper.left();
+      } */
+      
+       /*if (keyCode === 39) {
+        jumper.right();
+      } */
+      
+     //make a move function to move the ball right (if key is down)
      
+    
     }
